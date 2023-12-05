f932d1a923032009b6ae3adab814070ffab3498f
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-05 17:22:21 +0000

    Day 5 Part 2
    Best shift from part 1 to part 2 ever, AABB was the solution!

---
+++ Day 5/day5p2.py
@@ -0,0 +1,70 @@
+import math
+from itertools import groupby
+
+# https://www.reddit.com/r/adventofcode/s/lFc3UljtHJ
+
+file = open("input.txt")
+lines = file.read().splitlines()
+file.close()
+
+lines = [list(g) for k, g in groupby(lines, key=bool) if k]
+
+[seeds] = lines[0]
+seeds = list(map(int, seeds[seeds.index(':') + 2:].split()))
+seeds = [seeds[i:i +2] for i in range(0, len(seeds), 2)]
+seeds = [(s, s+l-1) for [s, l] in seeds]
+
+def parse_xy_map(xy_map):
+    return [(x, x+length-1, y-x) for y, x, length in xy_map]
+
+raw_maps = [[list(map(int, x.split())) for x in rm[1:]] for rm in lines[1:]]
+maps = [parse_xy_map(rm) for rm in raw_maps]
+
+# This feels very AABB collisions?...
+for map in maps:
+    # new seeds are seeds that have had a rule applied and are already shifted to the next stage
+    new_seeds = list()
+    for r1, r2, shift in map:
+        # remaining seeds are new ranges that got missed by a rule
+        remaining_seeds = list()
+        for s1, s2 in seeds:
+            if r2 < s1:
+                # The seeds are above the rule range
+                remaining_seeds.append((s1, s2))
+                continue
+            if s2 < r1:
+                # The seeds are below the rule range
+                remaining_seeds.append((s1, s2))
+                continue
+            if r1 <= s1 <= r2 <= s2:
+                # The end of the rules overlaps with the start of the seeds
+                new_seeds.append((s1 + shift, r2 + shift))
+                remaining_seeds.append((r2, s2))
+                continue
+            if s1 <= r1 <= r2 <= s2:
+                # All of the rule fits inside the seed
+                remaining_seeds.append((s1, r1))
+                new_seeds.append((r1 + shift, r2 + shift))
+                remaining_seeds.append((r2, s2))
+                continue
+            if s1 <= r1 <= s2 <= r2:
+                # The start of the rules overlaps with the end of the seeds
+                remaining_seeds.append((s1, r1))
+                new_seeds.append((r1 + shift, s2 + shift))
+                continue
+            if r1 <= s1 <= s2 <= r2:
+                # All of the seeds fit inside the rule
+                new_seeds.append((s1 + shift, s2 + shift))
+                continue
+            print('error', s1, s2, r1, r2)
+        # The next rule only needs to check over the seeds that no other rule has already mapped
+        seeds = remaining_seeds
+    # any rules that are unmapped remain the same
+    seeds.extend(new_seeds)
+
+# We only need to check the beginning of each seed range, as that is always the smallest
+locations = [seed[0] for seed in seeds]
+# Print the smallest of the locations
+print(min(locations))
+
+

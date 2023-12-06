0c247c7d25584e2185dcd362ddb147f895057328
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-06 13:06:35 +0000

    Day 5 Part 2 Fix+
    Renamed the seed lists to make more sense

--- Day 5/day5p2.py
+++ Day 5/day5p2.py
@@ -23,43 +23,43 @@ maps = [parse_xy_map(rm) for rm in raw_maps]
 # This feels very AABB collisions?...
 for map in maps:
     # new seeds are seeds that have had a rule applied and are already shifted to the next stage
-    new_seeds = list()
+    seeds_next_map = list()
     for r1, r2, shift in map:
         # remaining seeds are new ranges that got missed by a rule
-        remaining_seeds = list()
+        seeds_next_rule = list()
         for s1, s2 in seeds:
             if r2 < s1 or s2 < r1:
                 # There is no overlap between seeds and rule
-                remaining_seeds.append((s1, s2))
+                seeds_next_rule.append((s1, s2))
                 continue
             if r1 < s1 <= r2 < s2:
                 # The end of the rules overlaps with the start of the seeds
-                new_seeds.append((s1 + shift, r2 + shift))
-                remaining_seeds.append((r2 + 1, s2))
+                seeds_next_map.append((s1 + shift, r2 + shift))
+                seeds_next_rule.append((r2 + 1, s2))
                 continue
             if s1 < r1 <= s2 < r2:
                 # The start of the rules overlaps with the end of the seeds
-                remaining_seeds.append((s1, r1 - 1))
-                new_seeds.append((r1 + shift, s2 + shift))
+                seeds_next_rule.append((s1, r1 - 1))
+                seeds_next_map.append((r1 + shift, s2 + shift))
                 continue
             if r1 <= s1 <= s2 <= r2:
                 # All the seeds fit inside the rule
-                new_seeds.append((s1 + shift, s2 + shift))
+                seeds_next_map.append((s1 + shift, s2 + shift))
                 continue
             if s1 <= r1 <= r2 <= s2:
                 # All the rule fits inside the seed
                 if s1 != r1:
-                    remaining_seeds.append((s1, r1))
-                new_seeds.append((r1 + shift, r2 + shift))
+                    seeds_next_rule.append((s1, r1))
+                seeds_next_map.append((r1 + shift, r2 + shift))
                 if r2 != s2:
-                    remaining_seeds.append((r2, s2))
+                    seeds_next_rule.append((r2, s2))
                 continue
             # One of these cases should always be true
             raise Exception(f"AABB logic is incorrect for seeds: {s1, s2}, rule: {r1, r2}")
         # The next rule only needs to check over the seeds that no other rule has already mapped
-        seeds = remaining_seeds
+        seeds = seeds_next_rule
     # any rules that are unmapped remain the same
-    seeds.extend(new_seeds)
+    seeds.extend(seeds_next_map)
 
 # We only need to check the beginning of each seed range, as that is always the smallest
 locations = [seed[0] for seed in seeds]

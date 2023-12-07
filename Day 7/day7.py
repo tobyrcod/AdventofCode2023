aa2d46eff4ede857498c476a24a51982a4840027
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-07 13:15:14 +0000

    Huge Day 7 Part 2 Improvement

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -10,69 +10,25 @@ ALLOW_JOKERS = True
 rounds = (line.split(' ') for line in lines)
 rounds = ((r[0], int(r[1])) for r in rounds)
 
-def get_type_from_matching_counts(matching_counts, joker_count=0):
-    # This method works because it is never beneficial to turn multiple jokers into different things
-
-    def get_lowest_number_of_jokers_needed(target, jokers, mc=matching_counts):
-        # If we can use i jokers to reach the target t matching, return i
-        # If no amount of jokers will get us there, return -1
-        return next((i for i in range(0, jokers+1) if target-i in mc), -1)
-
-    # Can we trivially make 5 of a kind?
-    if joker_count >= 4:
-        return 6
-
-    # Can we make 5 by using n jokers?
-    n = get_lowest_number_of_jokers_needed(target=5, jokers=joker_count)
-    if n != -1:
-        # Five of a kind
-        return 6
-
-    # Can we make 4 by using n jokers?
-    n = get_lowest_number_of_jokers_needed(target=4, jokers=joker_count)
-    if n != -1:
+def get_type_from_matching_counts(matching_counts):
+    if len(matching_counts) == 1:
+        # Either Five of a kind or High card
+        if 5 in matching_counts:
+            # Five of a kind
+            return 6
+        # High card
+        return 0
+    if 4 in matching_counts:
         # Four of a kind
         return 5
-
-    # Can we make 3 by using n jokers?
-    n = get_lowest_number_of_jokers_needed(target=3, jokers=joker_count)
-    if n != -1:
-        """
-        We have a 3 of a kind
-        We can't have any jokers remaining
-        (if we did, we could trivially make a 4 of a kind)
-        """
-        # Remove the set of cards we made a Three of a Kind (or was already)
-        new_matching_counts = {key: val for (key, val) in matching_counts.items()}
-        new_matching_counts[3-n] -= 1
-        if new_matching_counts[3-n] == 0:
-            del new_matching_counts[3-n]
-
-        # Do we have a pair anywhere alongside the set of 3?
-        if 2 in new_matching_counts:
-            # Full House
+    if 3 in matching_counts:
+        # Either Full house or 3 of a kind
+        if 2 in matching_counts:
+            # Full house
             return 4
-        # Three of a Kind
+        # 3 of a kind
         return 3
-    """
-    At this stage, we know joker count is 0 or 1 
-    (if joker count was >= 2, we could trivially make a 3 of a kind)
-    
-    If we have a joker, we must have junk
-    (If we had a pair, then trivially with the joker we could make 3 of a kind)
-    
-    You cant have junk with a joker
-    (you can trivially make 1 pair)
-    """
-    if joker_count > 0:
-        # One Pair
-        return 1
-
-    # No jokers, so proceed as normal
-    if len(matching_counts) == 1:
-        # Junk / High Card
-        return 0
-
+    # Either Two Pair or One Pair
     if matching_counts[2] > matching_counts[1]:
         # Two pair
         return 2
@@ -81,14 +37,20 @@ def get_type_from_matching_counts(matching_counts, joker_count=0):
 
 def get_type_from_hand(hand):
     hand_counts = collections.Counter(hand)
-    joker_count = 0
+    # If we allow jokers, and we have some jokers
     if ALLOW_JOKERS and 'J' in hand_counts:
-        # If we allow jokers, and we have some jokers
+        # Turn all the jokers into the card we already have the most of that isn't a joker
+        # This trick only works because that strategy is always optimal for the hands we can make
         joker_count = hand_counts['J']
         del hand_counts['J']
-
+        if hand_counts:
+            hand_counts[max(hand_counts, key=hand_counts.get)] += joker_count
+        else:
+            # Edge case, if we had all jokers then we don't have a non-joker max
+            # So we just make our own, as we would turn all jokers into the best card we can
+            hand_counts['A'] = joker_count
     matching_counts = collections.Counter(hand_counts.values())
-    return get_type_from_matching_counts(matching_counts, joker_count)
+    return get_type_from_matching_counts(matching_counts)
 
 def types_comparison(types):
     type, hand, _ = types

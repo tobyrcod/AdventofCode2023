b569abb89e762429f9631db5bc94358c742ea46c
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-08 11:47:10 +0000

    Day 8 Part 2

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,3 +1,6 @@
+import re
+import numpy as np
+
 file = open("input.txt")
 lines = file.read().splitlines()
 file.close()
@@ -7,11 +10,22 @@ instructions = [0 if c == 'L' else 1 for c in lines[0]]
 edges = [line.split(" = ") for line in lines[2:]]
 network = {a: b[1:-1].split(", ") for [a, b] in edges}
 
-node = 'AAA'
-step = 0
-instructions_len = len(instructions)
-while node != 'ZZZ':
-    instruction = instructions[step % instructions_len]
-    node = network[node][instruction]
-    step += 1
-print(step)
+def part1(start_node='AAA', end_pattern=r'\bZZZ\b'):
+    node = start_node
+    step = 0
+    instructions_len = len(instructions)
+    end_pattern = re.compile(end_pattern)
+    while not end_pattern.match(node):
+        instruction = instructions[step % instructions_len]
+        node = network[node][instruction]
+        step += 1
+    return step
+
+def part2():
+    start_pattern = re.compile(r'\b\w{2}A\b')
+    start_nodes = filter(start_pattern.match, network)
+    cycles = map(lambda node: part1(start_node=node, end_pattern=r'\b\w{2}Z\b'), start_nodes)
+    return np.lcm.reduce(list(cycles))
+
+steps = part2()
+print(steps)

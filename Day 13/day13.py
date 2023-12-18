435db0f8adf672189f561f030265e78494ea036f
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-18 18:41:53 +0000

    Day 13 Fun Mini Golf Attempt

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -1,42 +1,22 @@
 import numpy as np
-
-# part1: NUM_ERRORS = 0, part2: NUM_ERRORS = 1
-NUM_ERRORS = 1
-
 file = open("input.txt")
 lines = file.read().split("\n\n")
+patterns = [np.asarray([list(row) for row in pattern]) for pattern in [line.split('\n') for line in lines]]
 file.close()
 
-patterns = [np.asarray([list(row) for row in pattern]) for pattern in [line.split('\n') for line in lines]]
+# ---------------------------
+# Fun Mini Golf Solution
+
+NUM_ERRORS = 1
 
 def hamming_distance(l, r):
     return sum(a != b for a, b in zip(l, r))
 
 def find_row_mirror(pattern):
-    return next((i for i in range(1, len(pattern)) if sum([hamming_distance(l, r) for l, r in zip(np.flip(pattern[:i], axis=0), pattern[i:])]) == NUM_ERRORS), -1)
+    return next((i for i in range(1, len(pattern)) if sum([hamming_distance(l, r) for l, r in zip(np.flip(pattern[:i], axis=0), pattern[i:])]) == NUM_ERRORS), 0)
 
 def solve():
-    total = 0
-    for pattern in patterns:
-        # Horizontal Mirror
-        mirror = find_row_mirror(pattern)
-        if mirror != -1:
-            total += 100 * mirror
-            continue
+    return sum(100*find_row_mirror(pattern) + find_row_mirror(pattern.T) for pattern in patterns)
 
-        # Vertical Mirror
-        mirror = find_row_mirror(pattern.T)
-        if mirror != -1:
-            total += mirror
-    print(total)
-
-solve()
-
-
-def tests():
-    assert list(zip([0, 1, 2], [3, 4, 5])) == [(0, 3), (1, 4), (2, 5)]
-    assert list(zip(reversed([0, 1, 2]), [3, 4, 5])) == [(2, 3), (1, 4), (0, 5)]
-    assert list(zip([0, 1], [3, 4, 5])) == [(0, 3), (1, 4)]
-    assert list(zip([0, 1, 2], [3, 4])) == [(0, 3), (1, 4)]
-    assert list(zip([0, 1, 2], [])) == []
+print(solve())
 

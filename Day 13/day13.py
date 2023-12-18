550949b4bc4ac92cbd9da21ffb17ecde73cdd322
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-18 18:27:35 +0000

    Day 13 Part 2

--- Day 13/day13.py
+++ Day 13/day13.py
@@ -1,16 +1,19 @@
-import sys
-
 import numpy as np
+
+# part1: NUM_ERRORS = 0, part2: NUM_ERRORS = 1
+NUM_ERRORS = 1
 
 file = open("input.txt")
 lines = file.read().split("\n\n")
 file.close()
 
-patterns = [np.asarray([list(row) for row in pattern]) for pattern in [np.array(line.split('\n')) for line in lines]]
+patterns = [np.asarray([list(row) for row in pattern]) for pattern in [line.split('\n') for line in lines]]
+
+def hamming_distance(l, r):
+    return sum(a != b for a, b in zip(l, r))
 
 def find_row_mirror(pattern):
-    # irow represents the row we are trying to see if it's the mirror
-    return next((i for i in range(1, len(pattern)) if all([all(l == r) for l, r in zip(np.flip(pattern[:i], axis=0), pattern[i:])])), -1)
+    return next((i for i in range(1, len(pattern)) if sum([hamming_distance(l, r) for l, r in zip(np.flip(pattern[:i], axis=0), pattern[i:])]) == NUM_ERRORS), -1)
 
 def solve():
     total = 0

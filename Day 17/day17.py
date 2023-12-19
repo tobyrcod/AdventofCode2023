e0b16aed51fb875ace3f063327367ef10d42f745
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-19 22:52:42 +0000

    Day 17 Part 1
    HUGE issues because my Node Class and Bearings were too slow, despite my logic for the problem actually being correct!

--- Day 17/day17.py
+++ Day 17/day17.py
@@ -1,118 +1,65 @@
 import heapq
-import math
-
 import numpy as np
-from typing import List
 
-with open('test1.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     lines = file.read().rsplit('\n')
 
-MAP = np.array([list(row) for row in lines], dtype=object)
+MAP = np.array([list(map(int, list(row))) for row in lines], dtype=int)
 HEIGHT, WIDTH = MAP.shape
-
-ROT_90 = math.radians(90)
-DIRECTION_CHAR = {
-    (-1, 0): "^",
-    (1, 0): "v",
-    (0, -1): "<",
-    (0, 1): ">"
-}
-
-def is_valid_position(position):
-    y, x = position
-    return 0 <= y < HEIGHT and 0 <= x < WIDTH
-
-def new_position(position, direction):
-    y, x = position
-    dy, dx = direction
-    return y + dy, x + dx
-
-class Node:
-    def __init__(self, position: tuple[int, int], bearing: float, bearing_count: int, cost: int, previous):
-        self.position = position
-        self.bearing = bearing
-        self.bearing_count = bearing_count
-        self.cost = cost
-        self.previous = previous
-
-    def __lt__(self, other):
-        return self.cost < other.cost
-
-    def __str__(self):
-        return f"{self.position} {self.cost} {DIRECTION_CHAR[get_bearing_direction(self.bearing)]}"
-
-    def __repr__(self):
-        return self.__str__()
-
-def get_bearing_direction(theta: float):
-    return int(-math.sin(theta)), int(math.cos(theta))
-
-def get_moves(node: Node):
-    theta = node.bearing
-
-    facing = get_bearing_direction(theta)
-    left = get_bearing_direction(theta + ROT_90)
-    right = get_bearing_direction(theta - ROT_90)
-
-    dirs = [(facing, theta), (left, theta+ROT_90), (right, theta-ROT_90)]
-    poss = [(new_position(node.position, dir), theta) for (dir, theta) in dirs]
-    return [(pos, theta) for (pos, theta) in poss if is_valid_position(pos)]
 
 class MinHeap:
     def __init__(self):
-        self.__elements: List[Node] = []
+        self.__elements = []
 
-    def push(self, node: Node) -> None:
-        heapq.heappush(self.__elements, node)
+    def push(self, *args):
+        heapq.heappush(self.__elements, args)
 
-    def pop(self) -> Node:
+    def pop(self):
         return heapq.heappop(self.__elements)
 
     def __bool__(self):
         return bool(self.__elements)
 
-    def __str__(self):
-        return str(self.__elements)
+def is_valid_position(position: tuple[int, int]):
+    return 0 <= position[0] < HEIGHT and 0 <= position[1] < WIDTH
 
-    def __len__(self):
-        return len(self.__elements)
+def add_vector2(a: tuple[int, int], b: tuple[int, int]):
+    return a[0] + b[0], a[1] + b[1]
 
-def find_min_path(end_pos: tuple[int, int]):
+def find_min_path(end_pos: tuple[int, int]) -> int:
     start_pos = (0, 0)
-    nodes = {start_pos: Node(start_pos, 0, 0, 0, None)}
+
     frontier = MinHeap()
-    frontier.push(nodes[start_pos])
+    frontier.push(0, start_pos, (0, 1), 0)
+    seen = set()
 
     while frontier:
-        current_node: Node = frontier.pop()
-        if current_node.position == end_pos:
-            path = list()
-            node = current_node
-            while node:
-                path.append(node)
-                node = node.previous
-            path.reverse()
-            return path
+        cost, position, direction, direction_count = frontier.pop()
 
-        for position, bearing in get_moves(current_node):
-            move_bearing_count = current_node.bearing_count + 1 if bearing == current_node.bearing else 1
-            # if move_bearing_count > 3:
-            #    continue
+        if position == end_pos:
+            return cost
 
-            cost = current_node.cost + int(MAP[position])
-            move_node = nodes.get(position, None)
-            if move_node and move_node.cost <= cost:
+        if (state := (position, direction, direction_count)) in seen:
+            continue
+
+        seen.add(state)
+
+        candidates = []
+        candidates.append((add_vector2(position, new_direction := (direction[1], -direction[0])), new_direction, 1))
+        candidates.append((add_vector2(position, new_direction := (-direction[1], direction[0])), new_direction, 1))
+        if direction_count < 3:
+            candidates.append((add_vector2(position, direction), direction, direction_count + 1))
+
+        for new_position, new_bearing, new_bearing_count in candidates:
+            if not is_valid_position(new_position):
                 continue
 
-            nodes[position] = Node(position, bearing, move_bearing_count, cost, current_node)
-            frontier.push(nodes[position])
+            new_cost = cost + MAP[new_position]
+            frontier.push(new_cost, new_position, new_bearing, new_bearing_count)
 
-    return None
+    return -1
 
 path = find_min_path((HEIGHT - 1, WIDTH - 1))
-if path:
-    for node in path:
-        MAP[node.position] = DIRECTION_CHAR[get_bearing_direction(node.bearing)]
+print(path)
 
-print(MAP)
 

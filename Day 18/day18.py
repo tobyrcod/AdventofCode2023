c1ad425adebc884dddd38c0967dfcfb6f2790444
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-20 03:39:33 +0000

    Day 18 Part 1
    Learnt about Picks and Shoelace theorem, as well as how to expand them to dealing with perimeter thickness

--- Day 18/day18.py
+++ Day 18/day18.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import List
+import numpy as np
 
-with open('test1.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     lines = file.read().rsplit('\n')
 
 @dataclass(frozen=True)
@@ -15,9 +16,9 @@ class Vector2:
 
     def __mul__(self, other: [int, float, Vector2]):
         if isinstance(other, (int, float)):
-            return Vector2(self.y * other, self.x * other)
+            return Vector2(self.x * other, self.y * other)
         if isinstance(other, Vector2):
-            return Vector2(self.y * other.y, self.x * other.x)
+            return Vector2(self.x * other.x, self.y * other.y)
         raise TypeError(f'Unsupported Operand for Vector2 Multiplication with {type(other)}')
 
     def __rmul__(self, other):
@@ -28,57 +29,75 @@ class Vector2:
 
     def __add__(self, other: [int, float, Vector2]):
         if isinstance(other, (int, float)):
-            return Vector2(self.y + other, self.x + other)
+            return Vector2(self.x + other, self.y + other)
         if isinstance(other, Vector2):
-            return Vector2(self.y + other.y, self.x + other.x)
+            return Vector2(self.x + other.x, self.y + other.y)
         raise TypeError(f'Unsupported Operand for Vector2 Addition with {type(other)}')
 
     def __sub__(self, other: [int, float, Vector2]):
         return self.__add__(-other)
 
     def __str__(self):
-        return f'({self.y}, {self.x})'
+        return f'({self.x}, {self.y})'
 
     def __repr__(self):
         return self.__str__()
 
-class Curve:
-    def __init__(self, points: List[Vector2] = None, closed: bool = True):
+class Polygon:
+    def __init__(self, points: List[Vector2] = None):
         if points is None:
             points = []
 
-        self.points = points
-        self.closed = closed
+        self.__points = points
 
     def add_point(self, point: Vector2):
         if not isinstance(point, Vector2):
             raise TypeError(f'Cannot add type {type(point)} to Curve')
-        self.points.append(point)
+        self.__points.append(point)
 
-    def calc_perimeter(self):
+    @property
+    def perimeter(self):
         perimeter = 0
-        length = len(self.points)
-        for i in range(1, length + 1 if self.closed else length):
-            perimeter += (self.points[i % length] - self.points[i - 1]).magnitude()
+        for i in range(len(self.__points)):
+            perimeter += (self.__points[i] - self.__points[i - 1]).magnitude()
         return perimeter
+
+    @property
+    def area(self):
+        # Shoelace formula (https://en.wikipedia.org/wiki/Shoelace_formula)
+        area = 0
+        length = len(self.__points)
+        for i in range(length):
+            a = self.__points[i]
+            b = self.__points[(i + 1) % length]
+            area += (a.x * b.y - b.x * a.y)
+        return abs(area / 2)
+
+    @property
+    def area_with_thickness(self):
+        # Need to adjust the area to include the thickness entire cells we are in, not just the points
+        # The entire perimeter needs to be expanded 0.5 out to reach the edge of the cell from the center it is now
+        # And then +1 for the external angles that needed to be expanded 0.75 each (360 degrees worth for a polygon)
+        return int(self.area + 0.5 * self.perimeter + 1)
+
+    def __str__(self):
+        return ', '.join([str(p) for p in self.__points])
 
 # (direction, meters, colour)
 dig_plan = [(d, int(n), c[2:-1]) for [d, n, c] in [line.split(' ') for line in lines]]
 
-direction_key_vector = {
-    'U': Vector2(-1, 0),
-    'D': Vector2(1, 0),
-    'L': Vector2(0, -1),
-    'R': Vector2(0, 1)
+directions = {
+    'U': Vector2(0, 1),
+    'D': Vector2(0, -1),
+    'L': Vector2(-1, 0),
+    'R': Vector2(1, 0)
 }
 
-curve = Curve()
+curve = Polygon()
 start_pos = Vector2(0, 0)
 current_pos = start_pos
-for (direction_key, distance, color) in dig_plan:
-    direction = direction_key_vector[direction_key]
-    current_pos += direction * distance
+for direction_key, distance, color in dig_plan:
+    current_pos += directions[direction_key] * distance
     curve.add_point(current_pos)
 
-print(curve.calc_perimeter())
-
+print(curve.area_with_thickness)

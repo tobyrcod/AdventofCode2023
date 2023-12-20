f6b3165b824749cfc33a7e3a5a580eab31f6c780
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-20 00:57:22 +0000

    Add Points to Curve

--- Day 18/day18.py
+++ Day 18/day18.py
@@ -26,10 +26,14 @@ class Vector2:
     def __neg__(self):
         return self.__mul__(-1)
 
-    def __add__(self, other: Vector2):
-        return Vector2(self.y + other.y, self.x + other.x)
+    def __add__(self, other: [int, float, Vector2]):
+        if isinstance(other, (int, float)):
+            return Vector2(self.y + other, self.x + other)
+        if isinstance(other, Vector2):
+            return Vector2(self.y + other.y, self.x + other.x)
+        raise TypeError(f'Unsupported Operand for Vector2 Addition with {type(other)}')
 
-    def __sub__(self, other: Vector2):
+    def __sub__(self, other: [int, float, Vector2]):
         return self.__add__(-other)
 
     def __str__(self):
@@ -39,8 +43,16 @@ class Vector2:
         return self.__str__()
 
 class Curve:
-    def __init__(self, points: List[Vector2]):
+    def __init__(self, points: List[Vector2]=None):
+        if points is None:
+            points = []
+
         self.points = points
+
+    def add_point(self, point: Vector2):
+        if not isinstance(point, Vector2):
+            raise TypeError(f'Cannot add type {type(point)} to Curve')
+        self.points.append(point)
 
     def calc_perimeter(self):
         perimeter = 0
@@ -59,14 +71,13 @@ direction_key_vector = {
     'R': Vector2(0, 1)
 }
 
-points = []
+curve = Curve()
 start_pos = Vector2(0, 0)
 current_pos = start_pos
 for (direction_key, distance, color) in dig_plan:
     direction = direction_key_vector[direction_key]
     current_pos += direction * distance
-    points.append(current_pos)
+    curve.add_point(current_pos)
 
-curve = Curve(points)
 print(curve.calc_perimeter())
 

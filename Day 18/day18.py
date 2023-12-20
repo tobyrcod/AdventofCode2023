7abd63c5e6274f9981932f998b5b2db68ecc1cc3
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-12-20 03:53:29 +0000

    Day 18 Part 2
    Luckily we just had to change the preprocessing, and it still runs instantly!

--- Day 18/day18.py
+++ Day 18/day18.py
@@ -6,6 +6,7 @@ import numpy as np
 with open('input.txt', 'r') as file:
     lines = file.read().rsplit('\n')
 
+
 @dataclass(frozen=True)
 class Vector2:
     x: float
@@ -42,6 +43,7 @@ class Vector2:
 
     def __repr__(self):
         return self.__str__()
+
 
 class Polygon:
     def __init__(self, points: List[Vector2] = None):
@@ -83,21 +85,35 @@ class Polygon:
     def __str__(self):
         return ', '.join([str(p) for p in self.__points])
 
+
 # (direction, meters, colour)
 dig_plan = [(d, int(n), c[2:-1]) for [d, n, c] in [line.split(' ') for line in lines]]
-
-directions = {
-    'U': Vector2(0, 1),
-    'D': Vector2(0, -1),
-    'L': Vector2(-1, 0),
-    'R': Vector2(1, 0)
-}
-
-curve = Polygon()
 start_pos = Vector2(0, 0)
-current_pos = start_pos
-for direction_key, distance, color in dig_plan:
-    current_pos += directions[direction_key] * distance
-    curve.add_point(current_pos)
 
-print(curve.area_with_thickness)
+def get_part1_poly():
+    poly = Polygon()
+
+    directions = {'U': Vector2(0, 1), 'D': Vector2(0, -1),
+                  'L': Vector2(-1, 0), 'R': Vector2(1, 0)}
+    current_pos = start_pos
+    for direction_key, distance, _ in dig_plan:
+        current_pos += directions[direction_key] * distance
+        poly.add_point(current_pos)
+
+    return poly
+
+def get_part2_poly():
+    poly = Polygon()
+
+    directions = {3: Vector2(0, 1), 1: Vector2(0, -1),
+                  2: Vector2(-1, 0), 0: Vector2(1, 0)}
+    current_pos = start_pos
+    for *_, hex in dig_plan:
+        distance, direction_key = int(hex[:-1], 16), int(hex[-1])
+        current_pos += directions[direction_key] * distance
+        poly.add_point(current_pos)
+
+    return poly
+
+poly = get_part2_poly()
+print(poly.area_with_thickness)
